2d41818613acb6b2e172ba082b03bc1cc8f41e07
TheOnlySmartBoy <MASENO@MASENO-PC>
2017-07-16 17:01:12 +0300

    Initial commit

--- includes/addp.php
+++ includes/addp.php
@@ -1,5 +1,5 @@
 <?php
-require_once '../core/init.php';
+$conn = mysqli_connect( "127.0.0.1", "root", "", "yuni_yuni" );
 $SC = $conn->escape_string($_POST['Schedule']);
 $dpname = $conn->escape_string($_POST['Unit']);
 $result = $conn->query("SELECT * FROM timetable WHERE Schedule = '$SC' AND Unit='$dpname'") or die($conn->error);
@@ -9,13 +9,14 @@ if ($result->num_rows > 0) {
     echo "That time and Room Is already Occupied";
         
 } else { 
-    $sql = "INSERT INTO timetable (Schedule,Unit) VALUES ('$dpname',$SC)";
+    $sql = "INSERT INTO timetable (Schedule,Unit) VALUES ('$SC','$dpname')";
     // Add user to the database
     if ($conn->query($sql)) {
         echo 'Timetable has been added successfully';
         
     } else {
-        echo 'Timetable has not been added please try again';
+        //echo 'Timetable has not been added please try again';
+        die($conn->error);
        
     }
 }
