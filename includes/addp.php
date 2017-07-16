6eab5c7af722a49ba34f571fba9235a09fbfab09
TheOnlySmartBoy <o2jose43@gmail.com>
2017-07-16 17:05:28 +0300

    Initial commit

--- includes/addp.php
+++ includes/addp.php
@@ -15,7 +15,7 @@ if ($result->num_rows > 0) {
         echo 'Timetable has been added successfully';
         
     } else {
-        //echo 'Timetable has not been added please try again';
+        echo 'Timetable has not been added please try again';
         die($conn->error);
        
     }
