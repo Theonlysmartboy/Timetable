05b4ac5f5faa7cd6df311b74e4d3100ee05e9332
TheOnlySmartBoy <MASENO@MASENO-PC>
2017-07-20 20:01:39 +0300

    Added Unit controls and views

--- functions/addunit.php
+++ functions/addunit.php
@@ -7,17 +7,17 @@ $prog = $conn->escape_string($_POST['prog']);
 $lec = $conn->escape_string($_POST['lec']);
 $result = $conn->query("SELECT * FROM units WHERE Unit_Code = '$code'") or die($conn->error);
 
-// school exists if the rows returned are more than 0
+// unit exists if the rows returned are more than 0
 if ($result->num_rows > 0) {
 
     echo 'The department already exists in the database!';
-} else { // School doesn't exist in a database, proceed...
+} else { // Unit doesn't exist in a database, proceed...
     // active is 0 by DEFAULT (no need to include it here)
     $sql = "INSERT INTO units (Unit_Name,Prog_Id,Unit_Code,Lecturer) VALUES ('$name','$prog','$code','$lec')";
     // Add user to the database
     if ($conn->query($sql)) {
         echo 'The department has been added successfully';
-       header("location: ../Units.php");
+        header("location: ../Units.php");
     } else {
         echo 'The department has not been added!';
         die($conn->error);
