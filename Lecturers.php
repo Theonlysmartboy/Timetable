42a2e58cea532a596bfe928cf67dceb8f16e2f9a
TheOnlySmartBoy <MASENO@MASENO-PC>
2017-07-20 21:28:33 +0300

    Finished function for adding lecturers

--- Lecturers.php
+++ Lecturers.php
@@ -1,17 +1,150 @@
+<?php
+require 'CORE/init.php';
+?>
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html>
     <head>
-        <meta charset="UTF-8">
-        <title></title>
+        <title>Lecturers</title>
+        <link rel="shortcut icon" href="images/timetable-menu-button-sm.png" type="image/x-icon"/>
+        <link href="css/bootstrap.css" rel="stylesheet" type="text/css"/>
+        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <link href="css/Appearance.css" rel="stylesheet" type="text/css"/>
+        <link href='http://fonts.googleapis.com/css?family=Titillium+Web:400,300,600' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+        <!--  JavaScript files-->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <link href="//fonts.googleapis.com/css?family=Cabin:400,400i,500,500i,600,600i,700,700i&amp;subset=latin-ext,vietnamese" rel="stylesheet">
+        <link href="//fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i&amp;subset=cyrillic,cyrillic-ext,greek,greek-ext,latin-ext,vietnamese" rel="stylesheet">
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+        <div class="row">
+            <div class="col-sm-3 col-md-3 well">
+                <div class="bg-info">
+                </div>
+                <div class="buttons">
+                    <ul style="list-style: none; float: contour;">
+                        <li style="margin-bottom: 10px;"><a href="index.php"><button class="btn btn-block bg-primary">Back</button></a></li>
+                        <li style="margin-bottom: 10px; margin-top: 10px;"><a href="Lecture halls.php"><button class="btn btn-block btn-success">Manage Lecture Halls </button></a></li>
+                        <li><a href="Lecturers.php"><button class="btn btn-block btn-success">Manage Lecturers </button></a></li>
+                    </ul>
+                </div>
+            </div> 
+            <div class="col-sm-9 col-md-9 well">
+                <div class="form">
+                    <ul class="tab-group">
+                        <li class="tab"><a href="#addschools">Add Lecturers</a></li>
+                        <li class="tab active"><a href="#viewschools">View Lecturers</a></li>
+                    </ul>
+                    <div class="tab-content">
+                        <div id="viewschools">   
+                            <?php
+                            $result = $conn->query("SELECT lecturers.Pf_No, lecturers.Title,lecturers.First_Name, lecturers.Middle_Name,"
+                                    . "lecturers.Last_Name, departments.Dept_Name FROM lecturers "
+                                    . "JOIN departments ON lecturers.Dept_Id=departments.Department_id order by Pf_No asc ");
+                            ?>
+                            <table width="500" border="0" cellspacing="1" cellpadding="0">
+                                <tr>
+                                    <td>
+                                        <form name="form1" method="post" action="">
+                                            <table class="table table-bordered table-striped table-hover table-responsive">
+                                                <thead>
+                                                    <tr class="bg-primary">
+                                                        <th>#</th>
+                                                        <th>PF NO</th>
+                                                        <th>Name</th>
+                                                        <th>DEPARTMENT</th>
+                                                        <th>action</th>
+                                                    </tr>
+                                                </thead>
+                                                <?php
+                                                $a = 1;
+                                                while ($row = mysqli_fetch_array($result)) {
+                                                    ?>
+                                                    <tbody>
+                                                        <tr>
+                                                            <td align="center"><?php echo $a++; ?></td>
+                                                            <td><?php echo $row['Pf_No']; ?></td>
+                                                            <td><?php echo $row['Title'] . " " . $row['First_Name'] . " " . $row['Middle_Name'] . " " . $row['Last_Name']; ?></td>
+                                                            <td><?php echo $row['Dept_Name']; ?></td>
+                                                            <td><a href='functions/deletelec.php?id=<?php echo $row['Pf_No']; ?>' class="btn btn-block btn-danger"> delete</a></td>
+                                                        </tr>
+                                                        <?php
+                                                    }
+                                                    $conn->close();
+                                                    ?>
+                                                </tbody>
+                                            </table>
+                                        </form>
+                                    </td>
+                                </tr>
+                            </table>
+                            ?>
+                        </div>
+                        <div id="addschools">   
+                            <h1 style="color: #FFFFFF;">Enter all the relevant details here</h1>
+
+                            <form action="functions/addlecs.php" method="post">
+                                <div class="top-row">
+                                <div class="form-group field-wrap">
+                                    <label>Title<span class="req">*</span>
+                                    </label>
+                                    <select name="title" id="title">
+                                        <option value="MR.">MR.</option>
+                                        <option value="MRS.">MRS.</option>
+                                    </select>
+                                </div>
+                                     <div class="form-group field-wrap">
+                                        <label>Pf Number <span class="req">*eg 0001</span>
+                                        </label>
+                                        <input type="text" required autocomplete="on" name='pfno' style="color: #FFFFFF;" />
+                                    </div>
+                                </div>
+                                <div class="top-row">
+                                    <div class="form-group field-wrap">
+                                        <label>First Name <span class="req">*</span>
+                                        </label>
+                                        <input type="text" required autocomplete="on" name='fname' style="color: #FFFFFF;" />
+                                    </div>
+                                    <div class="form-group field-wrap">
+                                        <label>Middle Name <span class="req">*</span>
+                                        </label>
+                                        <input type="text" required autocomplete="on" name='mname' style="color: #FFFFFF;" />
+                                    </div>
+                                </div>
+
+                                <div class="top-row">
+                                     <div class="form-group field-wrap">
+                                        <label>Last Name <span class="req">*</span>
+                                        </label>
+                                        <input type="text" required autocomplete="on" name='lname' style="color: #FFFFFF;" />
+                                    </div>
+                                    <div class="form-group field-wrap">
+                                        <label for="lec">Lecturer</label>
+                                        <select name="dept" id="dept" class="form-control">
+                                            <option value="Select School">Select Department</option>
+                                            <?php
+                                            require 'CORE/init.php';
+                                            $result1 = $conn->query("SELECT * FROM departments") or die($conn->error);
+                                            while ($row = mysqli_fetch_array($result1)) {
+                                                echo "<option value='" . $row['Department_id'] . "'>" . $row['Dept_Name'];
+                                                "</option>";
+                                            }
+                                            ?>        
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="form-group field-wrap">
+                                    <button type="submit" class="button button-block" name="register" />SUBMIT</button>
+                                </div>
+                            </form>
+                        </div>  
+                    </div><!-- tab-content -->
+                </div> <!-- /form -->
+            </div>
+        </div>
+        <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+        <script src="js/index.js" type="text/javascript"></script>
+        <script src="js/Copyright.js" type="text/javascript"></script>
     </body>
 </html>
--- register.php
+++ register.php
@@ -1,7 +1,7 @@
 <?php
 require_once 'core/init.php';
-if(Input::exists()){
-    if(Token::check(Input::get('token'))) {
+if (Input::exists()) {
+    if (Token::check(Input::get('token'))) {
         $validate = new Validate();
         $validation = $validate->check($_POST, array(
             'username' => array(
@@ -29,18 +29,19 @@ if(Input::exists()){
             $user = new User();
             $salt = Hash::salt(32);
 
-            try{
+            try {
                 $user->create(array(
                     'username' => Input::get('username'),
                     'password' => Hash::make(Input::get('password'), $salt),
                     'salt' => $salt,
                     'name' => Input::get('name'),
                     'joined' => date('Y-m-d H:i:s'),
-                    'group' => 1
+                    'group' => 1,
+                    'Department' => Input::get('dept')
                 ));
                 Session::flash('home', 'You have been registered Successfully you can now login');
                 Redirect::to('login.php');
-            }catch (Exception $e){
+            } catch (Exception $e) {
                 die($e->getMessage());
             }
         } else {
@@ -51,66 +52,81 @@ if(Input::exists()){
         }
     }
 }
-if ( !empty($big_errors) ) {
-	?>
-	<script type="text/javascript">
-		alert(<?php echo "('" . $big_errors . "')"; ?>);
-	</script>
-	<?php
-}else{
-	//Do nothing
+if (!empty($big_errors)) {
+    ?>
+    <script type="text/javascript">
+        alert(<?php echo "('" . $big_errors . "')"; ?>);
+    </script>
+    <?php
+} else {
+    //Do nothing
 }
 ?>
 <!DOCTYPE html>
 <html >
-  <head>
-    <meta charset="UTF-8">
-    <title>Sign Up to KCA Timetable</title>
+    <head>
+        <meta charset="UTF-8">
+        <title>Sign Up to KCA Timetable</title>
         <link rel="stylesheet" href="css/style.css">
-  </head>
+    </head>
 
-  <body>
-  <div style="margin-top: 40px; color: #000; text-align: center;">
-      <h3>Sign Up to KCA timetable or do you already have an<br> account with KCA timetable? <a href="login.php">Log In</a> </h3>
-  </div>
-    <div class="wrapper" style="border:none; min-height: 600px;">
-	<div class="container" >
-		<h1>Register</h1>
-		
-		<form class="form" action="" method="POST">
-			 <div class="field">
-        <label for="username">Username</label>
-        <input type="text" name="username" id="username" value="<?php echo escape(Input::get('username')); ?>" autocomplete="off">
+    <body>
+        <div style="margin-top: 40px; color: #000; text-align: center;">
+            <h3>Sign Up to KCA timetable or do you already have an<br> account with KCA timetable? <a href="login.php">Log In</a> </h3>
         </div>
-        <div class="field">
-            <label for="password">Choose Password</label>
-            <input type="password" name="password" id="password">
-        </div>
-        <div class="field">
-            <label for="password_again">Confirm Password</label>
-            <input type="password" name="password_again" id="password_again">
-        </div>
-        <div class="field">
-            <label for="name">Name</label>
-            <input type="text" name="name" id="name" value="<?php echo escape(Input::get('name')); ?>" autocomplete="off"><br>
-            <input type="hidden" name="token" value="<?php echo Token::generate(); ?>">
-        </div>
-			<button type="submit" id="login-button">Sign Up</button>
-		</form>
-	</div>
-	
-	<ul class="bg-bubbles">
-		<li>KCA LOGIN </li>
-		<li>PAGE</li>
-		<li>TIMETABLE</li>
-		<li>RESET PASSWORD</li>
-		<li>UPDATE DATA</li>
-		<li>KCA</li>
-		<li>TIMETABLE</li>
-		<li>PAGE</li>
-		<li>KCA LOGIN</li>
-		<li>RESET PASSWORD</li>
-	</ul>
-</div>   
-  </body>
+        <div class="wrapper" style="border:none; min-height: 600px;">
+            <div class="container" >
+                <h1>Register</h1>
+
+                <form class="form" action="" method="POST">
+                    <div class="field">
+                        <label for="username">Username</label>
+                        <input type="text" name="username" id="username" value="<?php echo escape(Input::get('username')); ?>" autocomplete="off">
+                    </div>
+                    <div class="field">
+                        <label for="lec">Program</label>
+                        <select name="dept" id="dept" class="form-control">
+                            <option value="">Select Department</option>
+                            <?php
+                            require 'CORE/init.php';
+                            $result1 = $conn->query("SELECT * FROM programs") or die($conn->error);
+                            while ($row = mysqli_fetch_array($result1)) {
+                                echo "<option value='" . $row['Prog_Id'] . "'>" . $row['Prog_name'];
+                                "</option>";
+                            }
+                            ?>        
+                        </select>
+                    </div>
+
+                    <div class="field">
+                        <label for="password">Choose Password</label>
+                        <input type="password" name="password" id="password">
+                    </div>
+                    <div class="field">
+                        <label for="password_again">Confirm Password</label>
+                        <input type="password" name="password_again" id="password_again">
+                    </div>
+                    <div class="field">
+                        <label for="name">Name</label>
+                        <input type="text" name="name" id="name" value="<?php echo escape(Input::get('name')); ?>" autocomplete="off"><br>
+                        <input type="hidden" name="token" value="<?php echo Token::generate(); ?>">
+                    </div>
+                    <button type="submit" id="login-button">Sign Up</button>
+                </form>
+            </div>
+
+            <ul class="bg-bubbles">
+                <li>KCA LOGIN </li>
+                <li>PAGE</li>
+                <li>TIMETABLE</li>
+                <li>RESET PASSWORD</li>
+                <li>UPDATE DATA</li>
+                <li>KCA</li>
+                <li>TIMETABLE</li>
+                <li>PAGE</li>
+                <li>KCA LOGIN</li>
+                <li>RESET PASSWORD</li>
+            </ul>
+        </div>   
+    </body>
 </html>
