6575274b4321b3c31a90e11ed961eb65db2210db
TheOnlySmartBoy <MASENO@MASENO-PC>
2017-07-20 17:39:38 +0300

    Edited the index page

--- js/index.js
+++ js/index.js
@@ -4,3 +4,54 @@
 	 $('form').fadeOut(500);
 	 $('.wrapper').addClass('form-success');
 });
+$('.form').find('input, textarea').on('keyup blur focus', function (e) {
+
+    var $this = $(this),
+            label = $this.prev('label');
+
+    if (e.type === 'keyup') {
+        if ($this.val() === '') {
+            label.removeClass('active highlight');
+        } else {
+            label.addClass('active highlight');
+        }
+    } else if (e.type === 'blur') {
+        if ($this.val() === '') {
+            label.removeClass('active highlight');
+        } else {
+            label.removeClass('highlight');
+        }
+    } else if (e.type === 'focus') {
+
+        if ($this.val() === '') {
+            label.removeClass('highlight');
+        }
+        else if ($this.val() !== '') {
+            label.addClass('highlight');
+        }
+    }
+
+});
+
+$('.tab a').on('click', function (e) {
+
+    e.preventDefault();
+
+    $(this).parent().addClass('active');
+    $(this).parent().siblings().removeClass('active');
+
+    target = $(this).attr('href');
+
+    $('.tab-content > div').not(target).hide();
+
+    $(target).fadeIn(600);
+
+});
+$(document).ready(function ViewStudents() {
+    if( document.getElementById('std-details').style.display === "none"){
+        document.getElementById('std-details').style.display = "block";
+    }
+    else{
+        document.getElementById('std-details').style.display = "none";
+    }
+});
